b5be7668823ab71797c8028800d34d5dd60bb1a2
Ahmed Saber <smartic@gmail.com>
2018-02-16 20:47:41 +0200

    Adding homepage route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { appRoutes } from './app.routes'
+// COMPONENTS
 import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component'
 
 
 @NgModule({
     declarations: [
-        LayoutComponent
+        LayoutComponent,
+        HomeComponent
     ],
     imports: [
+        RouterModule.forRoot(
+            appRoutes
+        ),
         BrowserModule
     ],
-    providers: [],
-    bootstrap: [LayoutComponent]
+    providers: [
+
+    ],
+    bootstrap: [
+        LayoutComponent
+    ]
 })
-export class AppModule { }
+export class AppModule {
+
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { IssueDetailsComponent } from './issue-details/issue-details.component'
+
+export const appRoutes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    }
+];
