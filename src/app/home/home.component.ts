869bfa0b54a68a104e6758ffd4c0fb0987c3a438
Ahmed Saber <smartic@gmail.com>
2018-02-16 20:59:05 +0200

    Homepage geting the list of issues

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+import { ApiHandlerService } from '../services/api-handler.service';
 
 @Component({
     selector: 'app-home',
@@ -7,10 +9,44 @@ import { Component } from '@angular/core';
 })
 
 export class HomeComponent {
-    constructor() {
+    page: number = 1;
+    details;
+    issuesList: any[] = [];
+    promise;
+
+    constructor(private ApiHandlerService: ApiHandlerService) {
     }
 
     ngOnInit() {
+        // GET TOTAL COUNT FOR OPEND ISSUES
+        this.promise = this.ApiHandlerService.getGithubIssuesDetails().subscribe((res) => {
+            this.details = res;
+            // GET FIRST PAGE
+            return this.getPage(1);
+        });
+    }
 
+    getPage(pageNum) {
+        // SET PAGE NUMBER
+        this.page = pageNum;
+        // GET ISSUES BY PAGE NUMBER
+        this.getListOfIssues(pageNum);
+    }
+
+    getListOfIssues(pageNum) {
+        // PARAMS
+        const params = new HttpParams()
+            .set('page', pageNum)
+            .set('state', 'all')
+            .set('direction', 'desc');
+        // RESET THE LIST
+        this.issuesList = [];
+        // CALL GITHUB ISSUES API
+        return this.ApiHandlerService.getGithubIssues({
+            params
+        }).subscribe((res: any[]) => {
+            // SAVE THE LIST
+            this.issuesList = res;
+        });
     }
 }
