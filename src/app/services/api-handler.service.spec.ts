464fe0cf989e25085c5d547ca368493c222a1678
Ahmed.Saber <smartic@gmail.com>
2018-02-19 13:44:35 +0200

    Testing api-handler.service methods real api calls

--- src/app/services/api-handler.service.spec.ts
+++ src/app/services/api-handler.service.spec.ts
@@ -1,29 +1,57 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
-
+import { HttpClient } from '@angular/common/http';
 import { ApiHandlerService } from './api-handler.service';
+import { HttpParams } from '@angular/common/http';
 
 describe('ApiHandlerService', () => {
+    // VARS
+    let service: ApiHandlerService;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [HttpClientModule],
-            providers: [ApiHandlerService]
+            imports: [
+                HttpClientModule,
+            ],
+            providers: [
+                ApiHandlerService
+            ]
+        });
+        service = TestBed.get(ApiHandlerService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should getGithubIssues return response', (done) => {
+        // PARAMS
+        const params = new HttpParams()
+            .set('page', '1')
+            .set('state', 'open')
+            .set('direction', 'desc');
+        service.getGithubIssues().subscribe(res => {
+            done();
+            expect(res['length']).toBeGreaterThan(1);
         });
     });
 
-    it('should be created', inject([ApiHandlerService], (service: ApiHandlerService) => {
-        expect(service).toBeTruthy();
-    }));
+    it('should getGithubIssuesDetails return the open_issues count', (done) => {
+        service.getGithubIssuesDetails().subscribe(res => {
+            done();
+            expect(res['open_issues']).toBeDefined();
+        });
+    });
 
-    it('should have getGithubIssues method', inject([ApiHandlerService], (service: ApiHandlerService) => {
-        expect(service.getGithubIssues).toBeTruthy();
-    }));
-
-    it('should have getGithubIssuesDetails method', inject([ApiHandlerService], (service: ApiHandlerService) => {
-        expect(service.getGithubIssuesDetails).toBeTruthy();
-    }));
-
-    it('should have getGithubIssueDetails method', inject([ApiHandlerService], (service: ApiHandlerService) => {
-        expect(service.getGithubIssueDetails).toBeTruthy();
-    }));
+    it('should getGithubIssueDetails return response', (done) => {
+        // PARAMS
+        const params = new HttpParams()
+            .set('page', '1')
+            .set('state', 'open')
+            .set('direction', 'desc');
+        service.getGithubIssueDetails(1).subscribe(res => {
+            done();
+            expect(res['title']).toBeTruthy();
+        });
+    });
 });
