45c84b77f23eb068850d11f30516c29af542edd0
Ahmed.Saber <smartic@gmail.com>
2018-02-19 14:13:57 +0200

    Adding typescript type for paramters

--- src/app/services/api-handler.service.ts
+++ src/app/services/api-handler.service.ts
@@ -1,21 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { resources } from '../resources';
+import { HttpHeaders } from '@angular/common/http/src/headers'
+import { HttpParams } from '@angular/common/http/src/params'
 
 @Injectable()
 export class ApiHandlerService {
 
     constructor(private http: HttpClient) { }
 
-    getGithubIssues(options = {}) {
+    getGithubIssues(options: options = {}) {
         return this.http.get(resources.githubIssuesAngular, options);
     }
 
-    getGithubIssuesDetails(options = {}) {
+    getGithubIssuesDetails(options: options = {}) {
         return this.http.get(resources.githubIssuesAngularDetails, options);
     }
 
-    getGithubIssueDetails(number, options = {}) {
-        return this.http.get(`${resources.githubIssueAngularDetails}/${number}`, options);
+    getGithubIssueDetails(number, options: options = {}) {
+        return this.http.get(`${resources.githubIssueAngularDetails}/${number}`);
     }
+}
+
+interface options {
+    headers?: HttpHeaders | {
+        [header: string]: string | string[];
+    };
+    observe?: 'body';
+    params?: HttpParams | {
+        [param: string]: string | string[];
+    };
+    reportProgress?: boolean;
+    responseType?: 'json';
+    withCredentials?: boolean;
 }
